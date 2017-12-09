5db1770b9ac8faabc2a26a370f4f8c0ba876d5c2
PauloSaicoski <pssarkis@inf.ufsm.br>
2017-12-09 10:52:10 -0200

    testa buffer remove tam

--- mainTest.c
+++ mainTest.c
@@ -7,13 +7,16 @@ int main(){
   a=3;
   int *x;
   x=&a;
-  buffer_inicializa(&b,50);
+  buffer_inicializa(&b,10);
+  printf ("proxInfTam %d\n", buffer_remove_tam(&b));
   printf ("tamanho %d\nlivre %d\nocupado %d\n", b.tam, b.livre, b.ocupado);
   printf ("insereTam %d\n", buffer_insere_tam(&b));
   buffer_insere(&b, (void*)x, sizeof(int));
+  printf ("proxInfTam %d\n", buffer_remove_tam(&b));
   printf ("tamanho %d\nlivre %d\nocupado %d\n", b.tam, b.livre, b.ocupado);
   printf ("insereTam %d\n", buffer_insere_tam(&b));
   buffer_finaliza(&b);
   printf ("finalizado\ntamanho %d\nlivre %d\nocupado %d\n", b.tam, b.livre, b.ocupado);
   printf ("insereTam %d\n", buffer_insere_tam(&b));
+  printf ("proxInfTam %d\n", buffer_remove_tam(&b));
 }
