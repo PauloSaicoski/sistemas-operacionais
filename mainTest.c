e9a3dc84ba85551dd7bb1e257d2d04ec33e4a0ad
PauloSaicoski <pssarkis@inf.ufsm.br>
2017-12-10 17:52:22 -0200

    mainTest com muitas insercoes e remocoes

--- mainTest.c
+++ mainTest.c
@@ -1,5 +1,7 @@
 #include "API1.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(){
   buffer b;
@@ -7,16 +9,73 @@ int main(){
   a=3;
   int *x;
   x=&a;
-  buffer_inicializa(&b,10);
-  printf ("proxInfTam %d\n", buffer_remove_tam(&b));
-  printf ("tamanho %d\nlivre %d\nocupado %d\n", b.tam, b.livre, b.ocupado);
-  printf ("insereTam %d\n", buffer_insere_tam(&b));
+  void *y;
+  y=malloc(sizeof(int));
+  int tam;
+  buffer_inicializa(&b,18);
   buffer_insere(&b, (void*)x, sizeof(int));
-  printf ("proxInfTam %d\n", buffer_remove_tam(&b));
-  printf ("tamanho %d\nlivre %d\nocupado %d\n", b.tam, b.livre, b.ocupado);
-  printf ("insereTam %d\n", buffer_insere_tam(&b));
+  buffer_insere(&b, (void*)x, sizeof(int));
+  buffer_remove(&b, y, sizeof(int), &tam);
+  printf ("%d\n", *((int*)y));
+  printf ("insere tam %d\n", buffer_insere_tam(&b));
+  printf ("remove tam %d\n\n", buffer_remove_tam(&b));
+  buffer_insere(&b, (void*)x, sizeof(int));
+  printf ("insere tam %d\n", buffer_insere_tam(&b));
+  printf ("remove tam %d\n\n", buffer_remove_tam(&b));
+  buffer_remove(&b, y, sizeof(int), &tam);
+  printf ("%d\n", *((int*)y));
+  printf ("insere tam %d\n", buffer_insere_tam(&b));
+  printf ("remove tam %d\n\n", buffer_remove_tam(&b));
+  buffer_insere(&b, (void*)x, sizeof(int));
+  printf ("insere tam %d\n", buffer_insere_tam(&b));
+  printf ("remove tam %d\n\n", buffer_remove_tam(&b));
+  buffer_remove(&b, y, sizeof(int), &tam);
+  printf ("%d\n", *((int*)y));
+  printf ("insere tam %d\n", buffer_insere_tam(&b));
+  printf ("remove tam %d\n\n", buffer_remove_tam(&b));
+  buffer_insere(&b, (void*)x, sizeof(int));
+  printf ("insere tam %d\n", buffer_insere_tam(&b));
+  printf ("remove tam %d\n\n", buffer_remove_tam(&b));
+  buffer_remove(&b, y, sizeof(int), &tam);
+  printf ("%d\n", *((int*)y));
+  printf ("insere tam %d\n", buffer_insere_tam(&b));
+  printf ("remove tam %d\n\n", buffer_remove_tam(&b));
+  buffer_remove(&b, y, sizeof(int), &tam);
+  printf ("%d\n", *((int*)y));
+  printf ("insere tam %d\n", buffer_insere_tam(&b));
+  printf ("remove tam %d\n\n", buffer_remove_tam(&b));
+  buffer_insere(&b, (void*)x, sizeof(int));
+  printf ("insere tam %d\n", buffer_insere_tam(&b));
+  printf ("remove tam %d\n\n", buffer_remove_tam(&b));
+  buffer_remove(&b, y, sizeof(int), &tam);
+  printf ("%d\n", *((int*)y));
+  printf ("insere tam %d\n", buffer_insere_tam(&b));
+  printf ("remove tam %d\n\n", buffer_remove_tam(&b));
+  buffer_remove(&b, y, sizeof(int), &tam);
+  printf ("%d\n", *((int*)y));
+  printf ("insere tam %d\n", buffer_insere_tam(&b));
+  printf ("remove tam %d\n\n", buffer_remove_tam(&b));
+  buffer_remove(&b, y, sizeof(int), &tam);
+  printf ("%d\n", *((int*)y));
+  printf ("insere tam %d\n", buffer_insere_tam(&b));
+  printf ("remove tam %d\n\n", buffer_remove_tam(&b));
+  buffer_remove(&b, y, sizeof(int), &tam);
+  printf ("%d\n", *((int*)y));
+  printf ("insere tam %d\n", buffer_insere_tam(&b));
+  printf ("remove tam %d\n\n", buffer_remove_tam(&b));
+  buffer_insere(&b, (void*)x, sizeof(int));
+  printf ("insere tam %d\n", buffer_insere_tam(&b));
+  printf ("remove tam %d\n\n", buffer_remove_tam(&b));
+  buffer_insere(&b, (void*)x, sizeof(int));
+  printf ("insere tam %d\n", buffer_insere_tam(&b));
+  printf ("remove tam %d\n\n", buffer_remove_tam(&b));
+  buffer_insere(&b, (void*)x, sizeof(int));
+  printf ("insere tam %d\n", buffer_insere_tam(&b));
+  printf ("remove tam %d\n\n", buffer_remove_tam(&b));
+
+
+
+  free(y);
   buffer_finaliza(&b);
-  printf ("finalizado\ntamanho %d\nlivre %d\nocupado %d\n", b.tam, b.livre, b.ocupado);
-  printf ("insereTam %d\n", buffer_insere_tam(&b));
-  printf ("proxInfTam %d\n", buffer_remove_tam(&b));
+  return 0;
 }
