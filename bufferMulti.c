9489522060d11adb4835ab10fdb27403d4aac270
PauloSaicoski <pssarkis@inf.ufsm.br>
2017-12-10 21:43:52 -0200

    corrigido b->buf.tam e inicializacao dos mutex

--- bufferMulti.c
+++ bufferMulti.c
@@ -8,10 +8,10 @@
 // Esta função só deve ser chamada uma vez para um buffer, e deve ser a 
 // primeira função chamada para esse buffer.
 bool bufc_inicializa(bufc *b, int cap){
-  b->escrita = PTHREAD_MUTEX_INITIALIZER;
-  b->leitura = PTHREAD_MUTEX_INITIALIZER;
-  b->escritaTam = PTHREAD_MUTEX_INITIALIZER;
-  b->leituraTam = PTHREAD_MUTEX_INITIALIZER;
+  pthread_mutex_init ( &(b->escrita), NULL);
+  pthread_mutex_init ( &(b->leitura), NULL);
+  pthread_mutex_init ( &(b->escritaTam), NULL);
+  pthread_mutex_init ( &(b->leituraTam), NULL);
   return buffer_inicializa(&(b->buff), cap);
 }
 // Finaliza um buffer previamente inicializado.
@@ -20,7 +20,7 @@ bool bufc_inicializa(bufc *b, int cap){
 // Essa função deve ser chamada uma única vez para um buffer.
 // Após esta chamada, o buffer não pode mais ser utilizado.
 void bufc_finaliza(bufc *b){
-  if(b->buff->tam < 0){
+  if(b->buff.tam < 0){
     return;
   }
   pthread_mutex_lock(&(b->escrita));
@@ -39,7 +39,7 @@ void bufc_finaliza(bufc *b){
 // retorna ``true`` quando o dado for inserido no buffer.
 bool bufc_insere(bufc *b, void *p, int tam){
   bool retorno;
-  if (tam > b->buff->tam){
+  if (tam > b->buff.tam){
     return false;
   }
   pthread_mutex_lock(&(b->escrita));
@@ -63,7 +63,7 @@ bool bufc_insere(bufc *b, void *p, int tam){
 // Retorna ``true`` quando for bem sucedido.
 bool bufc_remove(bufc *b, void *p, int cap, int *tam){
   bool retorno;
-  if(b->buff->tam < 0){
+  if(b->buff.tam < 0){
     return false;
   }
   if(buffer_remove_tam(&(b->buff)) < 0){
@@ -87,7 +87,7 @@ bool bufc_remove(bufc *b, void *p, int cap, int *tam){
 // Retorna ``true`` quando for bem sucedido.
 bool bufc_remove_malloc(bufc *b, void **p, int *tam){
   bool retorno;
-  if(b->buff->tam < 0){
+  if(b->buff.tam < 0){
     return false;
   }
   if(buffer_remove_tam(&(b->buff)) < 0){
